bf3b84f9b876ff7035bb7b1c7dc0a4cb22717c90
Gustavo Bazzo <gcbazzo@gmail.com>
2021-12-20 11:34:52 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021112200;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700;        // Requires this Moodle version.
+$plugin->requires  = 2020061500.00;        // Requires this Moodle version.
 $plugin->component = 'format_recit';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'R14-V1.0';
