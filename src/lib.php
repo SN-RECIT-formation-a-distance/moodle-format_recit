320cbb347b483fd9ae1b6856bb6aaad63baf4c85
Émile Jacques <emilevirus@gmail.com>
2022-06-06 13:08:30 -0400

    Fix boolean not saving

--- src/lib.php
+++ src/lib.php
@@ -414,14 +414,14 @@ class format_recit extends format_base {
         $changed = $needrebuild = false;
         foreach ($defaultoptions as $key => $value) {
             if (isset($records[$key])) {
+                if (in_array($key, array('ttsectionshowactivities','ttsectiontitle')) && !$data[$key]){
+                    $data[$key] = 0;
+                }
                 if (array_key_exists($key, $data) && $records[$key]->value !== $data[$key]) {
                     $value = $data[$key];
                     $newvalue = $data[$key];
                     if (is_array($data[$key])) {
                         $value = $data[$key]['text'];
-                    }
-                    if (in_array($key, array('ttsectionshowactivities','ttsectiontitle')) && !$value){
-                        $value = 0;
                     }
                     $DB->set_field('course_format_options', 'value', $value, array('id' => $records[$key]->id));
                     $changed = true;
