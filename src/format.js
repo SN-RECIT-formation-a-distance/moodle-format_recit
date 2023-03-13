285449d40f6b2f343320888b2bc7991b18c59a48
Émile Jacques <emilevirus@gmail.com>
2023-03-13 12:57:02 -0400

    Scroll to top when chsnging section

--- src/format.js
+++ src/format.js
@@ -606,6 +606,7 @@ M.recit.course.format.recit.NonEditingMode = class{
             if (section){
                 this.hideSections();
                 section.style.display = 'block';
+                window.scrollTo(0,0);
             }
         }
     }
