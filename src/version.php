8e5b94c947af3fe651ee6ed47e2725a8eb05ac39
Gustavo Bazzo <gcbazzo@gmail.com>
2023-10-19 14:45:58 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023011901;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023101900;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041900.00; // Moodle 4.0.0
 $plugin->component = 'format_recit';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
