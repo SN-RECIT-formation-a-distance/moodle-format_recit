07efc3c2a2325d1095f76e3c8e0433cfcd5b04a5
Gustavo Bazzo <gcbazzo@gmail.com>
2024-05-10 15:39:10 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -24,12 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024022200;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2024050900;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022041900.00; // Moodle 4.0.0
 $plugin->component = 'format_recit';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.1.8-stable';
-$plugin->supported = [400, 401];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
+$plugin->release = 'v3.1.9-stable';
+$plugin->supported = [400, 403];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 $plugin->dependencies = [
     'theme_recit2' => 2023011900
 ];
